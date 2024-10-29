d9b29b4e574ee650da4efb006732f571bfbbfeba
Szymon <szymon.gren93@gmail.com>
2024-10-29 12:49:47 +0100

    Update pg_backup.sh

--- pg_backup.sh
+++ pg_backup.sh
@@ -7,18 +7,10 @@ pgdumpall=/usr/pgsql-12/bin/pg_dumpall
 psql=/usr/pgsql-12/bin/psql
 
 
-/usr/pgsql-12/bin/psql -d postgres -c "SELECT datname FROM pg_catalog.pg_database WHERE datistemplate = false;"  > /"$backup_dir"/bazy.txt
+psql -d postgres -t -c "SELECT datname FROM pg_catalog.pg_database WHERE datistemplate = false;"  > /"$backup_dir"/bazy.txt
+sed -i "s/ //g" /"$backup_dir"/bazy.txt
+sed -i '$ d' /"$backup_dir"/bazy.txt
 
-#Liczba baz
-#batabase_count=$(wc -l < /migracjapg/bazy.txt)
-#echo $batabase_count
-#batabase_count=$(( $batabase_count - 2 ))
-#echo $batabase_count
-
-
-sed -i '$d' /"$backup_dir"/bazy.txt
-sed -i '$d' /"$backup_dir"/bazy.txt
-sed -i '1d;2d' /"$backup_dir"/bazy.txt
 
 mkdir -p /"$backup_dir"/backupy
 
@@ -30,7 +22,7 @@ while read p; do
 
 backupwana_baza=$p
 
-echo "Backupuję " $backupwana_baza
+echo "Backupuję bazę: " $backupwana_baza
 
 $pgdump -d "$backupwana_baza" -f "$backup_dir"/backupy/"$backupwana_baza".sql
 
