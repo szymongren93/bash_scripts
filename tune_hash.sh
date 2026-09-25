eb766f19df45da552d4e7766cdf7d82b785350d4
Szymon <szymon.gren93@gmail.com>
2026-09-25 11:36:06 +0200

    Update tune_hash.sh

--- tune_hash.sh
+++ tune_hash.sh
@@ -1,17 +1,49 @@
 #!/bin/bash
 
-LISTA="/migracjapg/params"
+LISTA="/install/params"
 CONF="/var/lib/pgsql/16/data/postgresql.conf"
-
 awk '
 NR==FNR {
-    wanted[$0]
+    # Zapamiętaj nazwę parametru i całą linię z params
+    if ($0 !~ /^[[:space:]]*#/ && $0 ~ /=/) {
+        line=$0
+        sub(/^[[:space:]]*/, "", line)
+        param=line
+        sub(/[[:space:]]*=.*/, "", param)
+        gsub(/[[:space:]]+$/, "", param)
+
+        wanted[param]=$0
+        order[++count]=param
+    }
     next
 }
 {
-    if ($0 in wanted && $0 !~ /^#/)
-        print "#" $0
-    else
-        print
+    # Sprawdź, czy linia jest aktywną konfiguracją
+    line=$0
+
+    if (line !~ /^[[:space:]]*#/ && line ~ /^[[:space:]]*[^[:space:]=]+[[:space:]]*=/) {
+        param=line
+        sub(/^[[:space:]]*/, "", param)
+        sub(/[[:space:]]*=.*/, "", param)
+        gsub(/[[:space:]]+$/, "", param)
+
+        if (param in wanted) {
+            print "#" $0
+            found[param]=1
+            next
+        }
+    }
+
+    print
+}
+END {
+    print ""
+    print "# Parameters from /install/params"
+
+    for (i=1; i<=count; i++) {
+        param=order[i]
+        print wanted[param]
+    }
 }
 ' "$LISTA" "$CONF" > "${CONF}.new" && mv "${CONF}.new" "$CONF"
+
